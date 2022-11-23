fff0621860bdbab621b2cb9c7cb14756d26c0aac
Student <venus.kabera@student.moringaschool.com>
2022-11-23 15:36:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1=2
+const num2=31
+
+const multiply=num1*num2
+
+const max=Math.max(1,2,3,4,20)
+
+
+const random=1
+const num3=19
+const num4=5
+const mod=num3%num4
